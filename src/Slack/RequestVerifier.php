37dd9acf22ea96800a60d633060e8c20f375f6fd
77web <info@77-web.com>
2019-10-14 09:17:36 +0900

    added RequestVerifier

---
+++ src/Slack/RequestVerifier.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace Quartetcom\SlackToGithubIssue\Slack;
+
+
+use Symfony\Component\HttpFoundation\Request;
+
+class RequestVerifier
+{
+    /**
+     * @var string
+     */
+    private $signSecret;
+
+    /**
+     * @param string $signSecret
+     */
+    public function __construct(string $signSecret)
+    {
+        $this->signSecret = $signSecret;
+    }
+
+    /**
+     * @param Request $request
+     * @throws VerificationFailureException
+     */
+    public function verify(Request $request)
+    {
+        $actualSignature = $request->headers->get('X-Slack-Signature');
+        if ($this->computeExpectedSignature($request) !== $actualSignature) {
+            throw new VerificationFailureException();
+        }
+    }
+
+    private function computeExpectedSignature(Request $request)
+    {
+        $requestTimestamp = $request->headers->get('X-Slack-Request-Timestamp');
+        $value = sprintf('v0:%s:%s', $requestTimestamp, $request->getContent());
+
+        return hash_hmac('sha256', $value, $this->signSecret);
+    }
+
+
+}
